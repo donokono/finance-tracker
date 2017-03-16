e636e6d03d313dba7474f913116416620dfd9195
Bob Scott <bob@zyzo.com>
2017-03-16 15:11:24 -0700

    Update views and nav.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => 'user/registrations' }
   resources :user_stocks, except: [:show, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-  get 'my_portfolio', to: "users#my_portfolio"
-  get 'search_stocks', to: "stocks#search"
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stocks', to: 'stocks#search'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
