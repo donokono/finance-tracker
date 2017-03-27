b4fad7948da8989fad625726b463d24dc4359db5
Bob Scott <bob@zyzo.com>
2017-03-27 18:59:50 +0000

    Add user search methods.

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,4 +1,4 @@
-class UserStocksController < ApplicationController
+ class UserStocksController < ApplicationController
   before_action :set_user_stock, only: [:show, :edit, :update, :destroy]
 
   # GET /user_stocks
